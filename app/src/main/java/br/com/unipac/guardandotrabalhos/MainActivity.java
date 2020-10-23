769a2caecf27bb62c39483297ef7f875e4a240a9
Matheus Gomes Andrade <matheus.andade2016@gmail.com>
2020-10-22 21:28:34 -0300

    Iniciando aplicação

--- app/src/main/java/br/com/unipac/guardandotrabalhos/MainActivity.java
+++ app/src/main/java/br/com/unipac/guardandotrabalhos/MainActivity.java
@@ -3,12 +3,25 @@ package br.com.unipac.guardandotrabalhos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nomeTrabalho;
+    EditText descTrabalho;
+    EditText dataEntregaTrabalho;
+    EditText pontuacaoTrabalho;
+    Spinner idDisciplinaTrabalho;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    List <Disciplinas> disciplinas = new ArrayList<>();
 }
